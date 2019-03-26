22ab36bba92648226558c50713f7836455abaa80
Enrico Weigelt, metux IT consult <info@metux.net>
2019-03-26 09:30:16 +0100

    1

--- jenkins-master/files/init.sh
+++ jenkins-master/files/init.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-/etc/init.d/postgresql start
-/etc/init.d/postfix start
-/etc/init.d/dbus start
+#/etc/init.d/postgresql start
+#/etc/init.d/postfix start
+#/etc/init.d/dbus start
 #/etc/init.d/firewalld start
-/etc/init.d/virtlogd start
-/etc/init.d/libvirtd start
+#/etc/init.d/virtlogd start
+#/etc/init.d/libvirtd start
 /etc/init.d/ssh start
 
-echo "CREATE USER jenkins CREATEDB PASSWORD 'jenkins001'" | su postgres -c psql
-echo "CREATE DATABASE jenkins OWNER = jenkins" | su postgres -c psql
+#echo "CREATE USER jenkins CREATEDB PASSWORD 'jenkins001'" | su postgres -c psql
+#echo "CREATE DATABASE jenkins OWNER = jenkins" | su postgres -c psql
 
 exec /usr/local/bin/jenkins.sh
 
